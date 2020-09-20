db84cf1cc267166e1e74e8394baa47986a537cf2
baselrabia <baselrabia@gmail.com>
2020-09-20 16:20:11 +0200

    List Rendering

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ const app = Vue.createApp({
       url: "https://www.vuemastery.com/",
       inventory: 9,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      // solution
+      sizes: ["S", "M", "L", "XL"],
+      // solution
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   }
 })
