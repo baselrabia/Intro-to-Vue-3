5f979fbb157e0d50f42e387125b86372d057ab3f
baselrabia <baselrabia@gmail.com>
2020-09-20 16:28:04 +0200

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart:0,
       product: "Socks",
       description: "A warm fuzzy pair of socks.",
       image: "./assets/socks_blue.jpg",
@@ -12,9 +13,24 @@ const app = Vue.createApp({
       sizes: ["S", "M", "L", "XL"],
       // solution
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/socks_blue.jpg" },
       ],
     };
-  }
+  },
+ methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        // solution
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        // solution
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
 })
