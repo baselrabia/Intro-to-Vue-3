7ad42f4e228d5e44b130a13f8fb0bb0804c41cbe
baselrabia <baselrabia@gmail.com>
2020-09-20 14:40:21 +0200

    nulled the index file

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    premium: true,
-    cart: [],
-  },
-  methods: {
-    updateCart(id) {
-      this.cart.push(id)
-    },
-  },
-})
+const product = "Socks";
